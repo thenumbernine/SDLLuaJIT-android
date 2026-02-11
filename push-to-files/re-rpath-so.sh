51e4a29fcb21d588ac26cc57aa274b848d20bd45
Christopher Moore <christopher.e.moore@gmail.com>
2026-02-11 13:14:38 +0700

    making rpath relative

--- push-to-files/re-rpath-so.sh
+++ push-to-files/re-rpath-so.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 # reset the rpath of a so to my SDLLuaJIT files folder
 # (is there another rpath I can use? maybe one thats relative or that depends on an env var? Anything that Android authors didn't intentionally cripple because they are dumb?)
-patchelf --set-rpath /data/data/io.github.thenumbernine.SDLLuaJIT/files "$1"
+# It looks like $ORIGIN is the /data/data/app/bin folder, such that $ORIGIN/../lib/ is the library folder ... which I probably can't write to ... but copying the .so's into files/ and setting rpath to $ORIGIN/.. seems to work
+patchelf --set-rpath "\$ORIGIN/.." "$1"
